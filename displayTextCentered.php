123ebbeef9e21aef8d2adb1c2eca80bd4f19b390
coinzzzpro <40394013+coinzzzpro@users.noreply.github.com>
2023-06-19 17:24:41 +0600

    Update displayTextCentered.php

--- displayTextCentered.php
+++ displayTextCentered.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+In this example, the displayTextCentered function takes five parameters: $imagePath (path to the image file), $text (the text to be displayed), $fontPath (path to the TrueType font file), $fontSize (size of the font), and $textColor (hex color code of the text).
+
+The function uses the GD library functions to load the image, calculate the position to center the text, allocate the text color, and write the text onto the image. Finally, it outputs the resulting image with the centered text.
+
+To use the function, provide the appropriate values for the example usage section. Set the $imagePath variable to the path of your image file, $text to the desired text, $fontPath to the path of the TrueType font file you want to use, $fontSize to the desired font size, and $textColor to the hex color code of the text color.
+
+The function assumes the image is in JPEG format, but you can modify the header function and imagejpeg function accordingly if your image is in a different format.
+
+Remember to ensure that you have the necessary GD library installed and enabled in your PHP configuration for this code to work properly.
+*/
+
 function displayTextCentered($imagePath, $text, $fontPath, $fontSize, $textColor)
 {
     // Load the image
